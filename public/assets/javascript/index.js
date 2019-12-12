ee1c3675c61cfd84b5ecb7d3527c3c533c2969b5
Jason Novak <novak.jason7@gmail.com>
2019-12-12 16:54:50 -0500

    Updated with new function to save articles using a save button.  Removed clear article function.  Displays articles from database when DOM is ready.

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -1,32 +1,40 @@
 scrapeArticles = () => $.get("/api/scrape");
 
 displayArticles = () => {
+
+    $(".article-container").empty();
     // grab the articles as json
     $.getJSON("/api/articles", function (data) {
 
         for (var i = 0; i < data.length; i++) {
             // display each article as a bootstrap card
-            $(".article-container").append("<div class='card m-4' data-id='" +
-                data[i]._id + "'><div class='card-body'><h5 class='card-title'>" +
-                data[i].headline + "</h5><p class='card-text'>" +
-                data[i].summary + "</p><a href='https://www.nytimes.com/" +
-                data[i].link + "' class='btn btn-primary' target='_blank'>Article</a></div></div>");
+            if (!data[i].saved) {
+                $(".article-container").append("<div class='card m-4' data-id='" +
+                    data[i]._id + "'><div class='card-body'><h5 class='card-title'>" +
+                    data[i].headline + "</h5><p class='card-text'>" +
+                    data[i].summary + "</p><a href='https://www.nytimes.com/" +
+                    data[i].link + "' class='btn btn-primary' target='_blank'>Article</a>" +
+                    "<button class='btn btn-info float-right save-article'>Save</button></div></div>");
+            }
         }
+        $(".save-article").on("click", saveArticle);
     });
 }
 
-clearArticles = () => {
-    // ajax request to clear database of all articles
-    $.get("/api/clear");
-    // removing articles from DOM
-    $(".article-container").empty();
+function saveArticle() {
+    let id = $(this).parent().parent().attr("data-id");
+
+    $.post("/api/save/" + id)
+        .then(function (data) {
+            // log the response
+            console.log(data);
+            displayArticles();
+        });
 }
 
 scrapeThenDisplay = () => {
-    $(".article-container").empty();
     scrapeArticles().then(displayArticles);
 }
 
 $(".scrape-new").on("click", scrapeThenDisplay);
-// $(".display-articles").on("click", displayArticles);
-$(".clear-articles").on("click", clearArticles);
+$(displayArticles);
